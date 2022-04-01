3ddf9b3f46c53be31d34a17aea07e1b3205af4ea
Sergio Morales <sergiogmr@icloud.com>
2022-04-01 10:54:18 +0100

    :rocket:

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { chromium } from 'playwright';
 import { MemoryCache } from 'cache-list';
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.HTTPS_PORT || 3000;
 
 const cache = new MemoryCache({
   defaultDuration: 600 // 10 min
